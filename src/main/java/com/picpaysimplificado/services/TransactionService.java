6c5180527668be34cb5ad19906cc24c944c22385
Lucas Duarte <duarte4lucas@gmail.com>
2024-02-25 11:30:44 -0300

    UserService concluido

---
+++ src/main/java/com/picpaysimplificado/services/TransactionService.java
@@ -0,0 +1,17 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.transaction.Transaction;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface TransactionService {
+    Transaction save(Transaction transaction);
+    Transaction update(Transaction transaction);
+    void delete(Long id);
+    Transaction findById(Long id);
+    List<Transaction> listAll();
+    List<Transaction> findByTimestamp(LocalDate timestamp);
+    List<Transaction> findByAmount(BigDecimal amount);
+}
