a3083071570d70327faff24934d148448f4d5d26
Lucas Duarte <duarte4lucas@gmail.com>
2024-02-25 11:30:53 -0300

    TransactionRepository concluido

--- src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
+++ src/main/java/com/picpaysimplificado/repositories/TransactionRepository.java
@@ -1,12 +1,13 @@
 package com.picpaysimplificado.repositories;
 
 import com.picpaysimplificado.domain.transaction.Transaction;
-import com.picpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findBySender(Long senderId);
-    List<Transaction> findByReceiver(Long receiverId);
+    List<Transaction> findByTimestamp(LocalDate timestamp);
+    List<Transaction> findByAmount(BigDecimal amount);
 }
